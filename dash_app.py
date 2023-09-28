db0e9685d4906e3984760886e101af2bdd900524
sam-walsh <64506503+sam-walsh@users.noreply.github.com>
2023-09-28 13:43:02 -0700

    Pitch clustering and CSW models

--- dash_app.py
+++ dash_app.py
@@ -1,16 +1,15 @@
 import dash
-from dash import dcc, html
-import dash_table
+from dash import dcc, html, dash_table
 import pandas as pd
 from dash.dependencies import Input, Output, State
-import dash_core_components as dcc
 import dash_bootstrap_components as dbc  # Import Bootstrap components
+from pybaseball import playerid_reverse_lookup
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])  # Add Bootstrap stylesheet
 
 # Load the data from model_predictions.csv
 df = pd.read_csv('pitcher_predictions.csv')
-leaderboards = df[['player_name', 'pitcher', 'team', 'pitch_type', 'pitch_score_percentile', 'pitches_thrown', 'pitch_usage']]
+leaderboards = df[['player_name', 'pitcher', 'pitch_type', 'pitch_score_percentile', 'pitches_thrown', 'pitch_usage']]
 leaderboards['pitch_score_percentile'] = leaderboards['pitch_score_percentile'].astype(int)
 
 # Create a dropdown to select a pitcher by name
@@ -31,14 +30,6 @@ pitches_slider = dcc.Slider(
 
 # Create a callback to update the image based on the selected pitcher
 @app.callback(
-    Output('pitcher-image', 'src'),
-    [Input('pitcher-dropdown', 'value')]
-)
-def update_image(value):
-    return f"https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_213,q_auto:best/v1/people/{value}/headshot/67/current"
-
-# Create a callback to update the pitch score percentile, pitch type and pitch usage based on the selected pitcher
-@app.callback(
     Output('pitcher-info', 'children'),
     [Input('pitcher-dropdown', 'value')]
 )
@@ -46,20 +37,54 @@ def update_pitcher_info(value):
     pitcher_info = leaderboards[leaderboards['pitcher'] == value][['pitch_type', 'pitch_score_percentile', 'pitch_usage']]
     pitcher_info = pitcher_info.sort_values('pitch_usage', ascending=False)  # Sort by pitch usage
     pitcher_info['pitch_usage'] = pitcher_info['pitch_usage'].apply(lambda x: f"{x}%")  # Convert pitch usage to percentage after sorting
-    # Use Bootstrap Card to present the pitcher info in a more visually appealing way
-    return dbc.Card([
-        dbc.CardHeader("Pitcher Info"),
-        dbc.CardBody([
-            html.Table([
-                html.Thead(
-                    html.Tr([html.Th('Pitch'), html.Th('Percentile'), html.Th('Usage')])
-                ),
-                html.Tbody([
-                    html.Tr([html.Td(row['pitch_type']), html.Td(row['pitch_score_percentile']), html.Td(row['pitch_usage'])]) for index, row in pitcher_info.iterrows()
-                ])
-            ])
-        ])
-    ])
+
+    # Create a list to hold all the cards
+    cards = []
+
+    # Create a card for the selected pitcher
+    player_name = leaderboards[leaderboards['pitcher'] == value]['player_name'].values[0]
+    first_name, last_name = player_name.split(", ")
+    formatted_name = f"{last_name} {first_name}"
+
+    pitch_cards = []
+
+    card = dbc.Card(
+        dbc.CardBody(
+            [
+                html.Div([
+                    html.Img(src=f"https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_213,q_auto:best/v1/people/{value}/headshot/67/current",
+                             alt='Player Image', style={'height':'150px', 'width':'100px', 'display': 'block', 'margin-left': 'auto', 'margin-right': 'auto'}),
+                    html.H5(formatted_name, className="card-title", style={'textAlign': 'center', 'display': 'block', 'margin-left': 'auto', 'margin-right': 'auto'})
+                ], style={'display': 'block', 'textAlign': 'center'})
+            ]
+        ),
+        className="mb-3",
+        style={"maxWidth": "200px", 'align': 'center'},
+        color="light"
+    )
+    cards.append(card)
+
+    # Create a card for each pitch
+    for index, row in pitcher_info.iterrows():
+        card = dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H4(row['pitch_type'], className="card-title", style={'textAlign': 'center'}),
+                    html.H1(f"{row['pitch_score_percentile']}", className="card-text", style={'textAlign': 'center'}),
+                    html.H5(f"{row['pitch_usage']}", className="card-text", style={'textAlign': 'center'})
+                ]
+            ),
+            className="mb-3",
+            style={"width": "150px", "height": "150px"},  # Make the cards square
+            color="light"
+        )
+        # Wrap each card in a dbc.Col component
+        pitch_cards.append(card)
+
+    pitch_cards_row = dbc.Row(pitch_cards)
+    cards.append(pitch_cards_row)
+
+    return cards
 
 # Create checkboxes for each pitch type
 pitch_types = leaderboards['pitch_type'].unique().tolist()
@@ -90,6 +115,7 @@ app.layout = html.Div(children=[
         html.Img(id='pitcher-image'),
         html.Div(id='pitcher-info'),  # Add a div to display the pitcher info
     ]),
+    html.Br(),
 
     html.H2(children='Leaderboard'),
     html.Div(children='''Select pitch types:'''),
--- feature_engineering.py
+++ feature_engineering.py
@@ -1,11 +1,34 @@
+def compute_approach_angles(df):
+    import numpy as np
+    yf = 17/12
+    df['vy_f'] = -np.sqrt(df['vy0']**2 - (2 * df['ay'] * (50 - yf)))
+    df['t'] = (df['vy_f'] - df['vy0']) / df['ay']
+    df['vz_f'] = df['vz0'] + (df['az'] * df['t'])
+    df['vx_f'] = df['vx0'] + (df['ax'] * df['t'])
+
+    df['vaa'] = -np.arctan(df['vz_f']/df['vy_f']) * (180 / np.pi)
+    df['haa'] = np.arctan(df['vx_f']/df['vy_f']) * (180 / np.pi)
+
+    df['vaa2'] = np.arctan((2.5 - df['release_pos_z']) / (df['release_pos_y'])) * (180 / np.pi)
+    df['haa2'] = np.arctan((0 - df['release_pos_x']) / (df['release_pos_y'])) * (180 / np.pi)
+    return df
+
+def compute_adjusted_axis_deviation(df):
+    import numpy as np
+    df['axis_deviation_adj'] = np.where(df['p_throws']=='L', df['diff_measured_inferred'].mul(-1), df['diff_measured_inferred'])
+    return df
+
 def compute_fastball_relative_features(df):
-    fastball_metrics = df.groupby(['player_name', 'pitcher', 'game_pk', 'pitch_type'])[['release_speed', 'hawkeye_measured', 'active_spin_formatted', 'az', 'ax']].mean().round().reset_index()
+    import numpy as np
+    fastball_metrics = df.groupby(['player_name', 'pitcher', 'game_pk', 'pitch_type'])[['release_speed', 'hawkeye_measured', 'active_spin_formatted', 'pfx_x', 'pfx_z', 'vaa', 'haa']].mean().round().reset_index()
     fastball_metrics = fastball_metrics.rename(columns={
         'release_speed': 'avg_top_velocity',
         'hawkeye_measured':'fb_spin_axis',
         'active_spin_formatted':'fb_active_spin',
         'az':'fastball_vert',
-        'ax':'fastball_horz'})
+        'ax':'fastball_horz',
+        'vaa':'fastball_vaa',
+        'haa':'fastball_haa'})
 
 
     fastball_metrics = fastball_metrics.dropna()
@@ -15,20 +38,9 @@ def compute_fastball_relative_features(df):
     df = df.merge(fastball_metrics_max_velocity, on=['player_name', 'pitcher', 'game_pk'], how='left')
 
     df['velo_delta'] = df['avg_top_velocity'] - df['release_speed']
-    df['spin_axis_delta'] = df['hawkeye_measured'] - df['fb_spin_axis']
+    df['spin_axis_delta'] = np.abs(df['hawkeye_measured'] - df['fb_spin_axis'])
 
-    df['vert_delta'] = df['fastball_vert'] - df['az']
-    df['horz_delta'] = df['fastball_horz'] - df['ax']
+    df['vert_delta'] = np.abs(df['fastball_vert'] - df['pfx_z'])
+    df['horz_delta'] = np.abs(df['fastball_horz'] - df['pfx_x'])
 
-    return df
-
-def compute_approach_angles(df):
-    import numpy as np
-    df['vaa'] = np.arctan((2.5 - df['release_pos_z']) / (df['release_pos_y'])) * (180 / np.pi)
-    df['haa'] = np.arctan((0 - df['release_pos_x']) / (df['release_pos_y'])) * (180 / np.pi)
-    return df
-
-def compute_adjusted_axis_deviation(df):
-    import numpy as np
-    df['axis_deviation_adj'] = np.where(df['p_throws']=='L', df['diff_measured_inferred'].mul(-1), df['diff_measured_inferred'])
     return df
